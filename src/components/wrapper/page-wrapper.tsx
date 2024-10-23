40446700160b915bb70bd3966d29d369e662b34e
Natalia Stelmach <natalia.stelmach@dynatrace.com>
2024-10-23 20:08:44 +0200

    Add wrapper

---
+++ src/components/wrapper/page-wrapper.tsx
@@ -0,0 +1,7 @@
+import Flex from "@/components/typohgraphy/flex";
+
+export default function PageWrapper({children}) {
+    return <Flex className="flex-col justify-center items-center lg:min-h-[calc(100vh-553px)] p-5 gap-4">
+        {children}
+    </Flex>
+}
