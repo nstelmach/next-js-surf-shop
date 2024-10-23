5b3971eaffcb5ed48d77eceac1eaf841bc4155df
Natalia Stelmach <natalia.stelmach@dynatrace.com>
2024-10-23 20:08:32 +0200

    Add order history

---
+++ src/components/order-history/history-details.tsx
@@ -0,0 +1,11 @@
+import Paragraph from "@/components/typohgraphy/paragraph";
+import Link from "next/link";
+
+export default function HistoryDetails() {
+    return <>
+        <Paragraph className="m-2 xl:text-base text-xl md:text-2xl text-center">You have not placed any
+            orders.</Paragraph>
+        <Link href="/my-account" className="hover:underline xl:text-base text-xl md:text-2xl text-center">Back to your
+            account</Link>
+    </>
+}
