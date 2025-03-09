f74e9e875b9db2ee9aa464a6b7dd52b13efa676f
Natalia Stelmach <natalia.stelmach@dynatrace.com>
2025-03-09 20:31:32 +0100

    Fix skeleton

--- src/app/(products)/components/product-list/list-skeleton.tsx
+++ src/app/(products)/components/product-list/list-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/src/components/ui/skeleton"
 export default function ListSkeleton() {
   return (
     <div className="w-screen h-screen flex justify-center items-start">
-      <div className="grid xl:grid-cols-3 grid-cols-1 md:place-items-center w-full h-full lg:w-2/3 lg:h-2/3 gap-8 p-4">
+      <div className="grid xl:grid-cols-3 grid-cols-1 md:place-items-center w-full h-full lg:w-3/4 lg:h-2/3 gap-8 p-4">
         <Skeleton className="h-[400px] md:w-[600px] md:h-[700px] lg:w-[400px] lg:h-[600px]" />
         <Skeleton className="h-[400px] md:w-[600px] md:h-[700px] lg:w-[400px] lg:h-[600px]" />
         <Skeleton className="h-[400px] md:w-[600px] md:h-[700px] lg:w-[400px] lg:h-[600px]" />
