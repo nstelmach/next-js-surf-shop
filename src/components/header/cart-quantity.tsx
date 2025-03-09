9a5118ec3390df8e84f49e869bfab4894002e633
Natalia Stelmach <natalia.stelmach@dynatrace.com>
2025-03-09 21:02:03 +0100

    Fix cart badge

--- src/components/header/cart-quantity.tsx
+++ src/components/header/cart-quantity.tsx
@@ -11,11 +11,11 @@ export default function CartQuantity() {
 
   return (
     <>
-      {totalQuantity && totalQuantity > 0 && (
+      {totalQuantity && totalQuantity > 0 ? (
         <div className="absolute top-[-5px] right-[-5px] bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
           {totalQuantity}
         </div>
-      )}
+      ) : null}
     </>
   )
 }
